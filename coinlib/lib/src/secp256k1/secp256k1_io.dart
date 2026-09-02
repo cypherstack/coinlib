ed4ccf323a60fc0612a77f8fc4c1dd59227ca203
sneurlax <sneurlax@gmail.com>
2026-09-02 17:08:29 -0500

    refactor: bind secp256k1 through the native asset
    Regenerate the bindings with ffi-native so functions are top-level
    @Native declarations resolved against the code asset from the build
    hook. The wrapper no longer loads a prebuilt library from build/ or via
    DynamicLibrary.open, and the path dependency is removed.

--- coinlib/lib/src/secp256k1/secp256k1_io.dart
+++ coinlib/lib/src/secp256k1/secp256k1_io.dart
@@ -1,41 +1,9 @@
 import "dart:ffi";
-import "dart:io";
 import "package:coinlib/src/crypto/random.dart";
 import 'package:ffi/ffi.dart';
 import "heap_array_ffi.dart";
 import "secp256k1.ffi.g.dart";
-import "package:path/path.dart";
 import "secp256k1_base.dart";
-
-const _name = "secp256k1";
-
-String _libraryPath() {
-
-  final String localLib, flutterLib;
-  if (Platform.isLinux || Platform.isAndroid) {
-    flutterLib = localLib = "lib$_name.so";
-  } else if (Platform.isMacOS || Platform.isIOS) {
-    // Dylib if built in build directory, or framework if using flutter
-    localLib = "lib$_name.dylib";
-    flutterLib = "$_name.framework/$_name";
-  } else if (Platform.isWindows) {
-    flutterLib = localLib = "$_name.dll";
-  } else {
-    throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
-  }
-
-  // Exists in build directory?
-  final libBuildPath = join(Directory.current.path, "build", localLib);
-  if (File(libBuildPath).existsSync()) {
-    return libBuildPath;
-  }
-
-  // Load from flutter library name
-  return flutterLib;
-
-}
-
-DynamicLibrary _openLibrary() => DynamicLibrary.open(_libraryPath());
 
 /// Specialises Secp256k1Base to use the FFI
 class Secp256k1 extends Secp256k1Base<
@@ -51,39 +19,37 @@ class Secp256k1 extends Secp256k1Base<
   Pointer<Never>
 > {
 
-  final _lib = NativeSecp256k1(_openLibrary());
-
   Secp256k1() {
 
     // Set functions
-    extEcSeckeyVerify = _lib.secp256k1_ec_seckey_verify;
-    extEcPubkeyCreate = _lib.secp256k1_ec_pubkey_create;
-    extEcPubkeySerialize = _lib.secp256k1_ec_pubkey_serialize;
-    extEcPubkeyParse = _lib.secp256k1_ec_pubkey_parse;
-    extEcdsaSign = _lib.secp256k1_ecdsa_sign;
+    extEcSeckeyVerify = secp256k1_ec_seckey_verify;
+    extEcPubkeyCreate = secp256k1_ec_pubkey_create;
+    extEcPubkeySerialize = secp256k1_ec_pubkey_serialize;
+    extEcPubkeyParse = secp256k1_ec_pubkey_parse;
+    extEcdsaSign = secp256k1_ecdsa_sign;
     extEcdsaSignatureSerializeCompact
-      = _lib.secp256k1_ecdsa_signature_serialize_compact;
+      = secp256k1_ecdsa_signature_serialize_compact;
     extEcdsaSignatureParseCompact
-      = _lib.secp256k1_ecdsa_signature_parse_compact;
-    extEcdsaSignatureNormalize = _lib.secp256k1_ecdsa_signature_normalize;
+      = secp256k1_ecdsa_signature_parse_compact;
+    extEcdsaSignatureNormalize = secp256k1_ecdsa_signature_normalize;
     extEcdsaSignatureSerializeDer =
-      _lib.secp256k1_ecdsa_signature_serialize_der;
-    extEcdsaSignatureParseDer = _lib.secp256k1_ecdsa_signature_parse_der;
-    extEcdsaVerify = _lib.secp256k1_ecdsa_verify;
+      secp256k1_ecdsa_signature_serialize_der;
+    extEcdsaSignatureParseDer = secp256k1_ecdsa_signature_parse_der;
+    extEcdsaVerify = secp256k1_ecdsa_verify;
     extEcdsaRecoverableSignatureSerializeCompact
-      = _lib.secp256k1_ecdsa_recoverable_signature_serialize_compact;
+      = secp256k1_ecdsa_recoverable_signature_serialize_compact;
     extEcdsaRecoverableSignatureParseCompact
-      = _lib.secp256k1_ecdsa_recoverable_signature_parse_compact;
-    extEcdsaSignRecoverable = _lib.secp256k1_ecdsa_sign_recoverable;
-    extEcdsaRecover = _lib.secp256k1_ecdsa_recover;
-    extEcSeckeyTweakAdd = _lib.secp256k1_ec_seckey_tweak_add;
-    extEcPubkeyTweakAdd = _lib.secp256k1_ec_pubkey_tweak_add;
-    extEcSeckeyNegate = _lib.secp256k1_ec_seckey_negate;
-    extKeypairCreate = _lib.secp256k1_keypair_create;
-    extXOnlyPubkeyParse = _lib.secp256k1_xonly_pubkey_parse;
-    extSchnorrSign32 = _lib.secp256k1_schnorrsig_sign32;
-    extSchnorrVerify = _lib.secp256k1_schnorrsig_verify;
-    extEcdh = _lib.secp256k1_ecdh;
+      = secp256k1_ecdsa_recoverable_signature_parse_compact;
+    extEcdsaSignRecoverable = secp256k1_ecdsa_sign_recoverable;
+    extEcdsaRecover = secp256k1_ecdsa_recover;
+    extEcSeckeyTweakAdd = secp256k1_ec_seckey_tweak_add;
+    extEcPubkeyTweakAdd = secp256k1_ec_pubkey_tweak_add;
+    extEcSeckeyNegate = secp256k1_ec_seckey_negate;
+    extKeypairCreate = secp256k1_keypair_create;
+    extXOnlyPubkeyParse = secp256k1_xonly_pubkey_parse;
+    extSchnorrSign32 = secp256k1_schnorrsig_sign32;
+    extSchnorrVerify = secp256k1_schnorrsig_verify;
+    extEcdh = secp256k1_ecdh;
 
     // Set heap arrays
     key32Array = HeapArrayFfi(Secp256k1Base.privkeySize);
@@ -108,7 +74,7 @@ class Secp256k1 extends Secp256k1Base<
     nullPtr = nullptr;
 
     // Create context
-    ctxPtr = _lib.secp256k1_context_create(Secp256k1Base.contextNone);
+    ctxPtr = secp256k1_context_create(Secp256k1Base.contextNone);
 
     // Randomise context with 32 bytes
 
@@ -116,7 +82,7 @@ class Secp256k1 extends Secp256k1Base<
     final randArray = HeapArrayFfi(32);
     randArray.load(randBytes);
 
-    if (_lib.secp256k1_context_randomize(ctxPtr, randArray.ptr) != 1) {
+    if (secp256k1_context_randomize(ctxPtr, randArray.ptr) != 1) {
       throw Secp256k1Exception("Secp256k1 context couldn't be randomised");
     }
 
